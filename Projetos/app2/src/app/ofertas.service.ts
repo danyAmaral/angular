12235d51c0fa7ed2aa0b12def1635ccc5466798c
danyAmaral <danyamaral27@gmail.com>
2019-10-09 22:06:04 -0300

    add rotas

--- Projetos/app2/src/app/ofertas.service.ts
+++ Projetos/app2/src/app/ofertas.service.ts
@@ -8,10 +8,18 @@ export class OfertasService{
 constructor(private http: HttpClient){}
 
     public getOfertas(): Promise<Oferta[]>{
-        return  this.http.get("http://localhost:3000/ofertas")
+        return  this.http.get("http://localhost:3000/ofertas?destaque=true")
             .toPromise()
             .then((resposta: any) => {
                 return resposta;
             })
+    }
+
+    public getOfertasPorCategoria(categoria: string): Promise<Oferta[]>{
+        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+                .toPromise()
+                .then((resposta: any) => {
+                    return resposta;
+                })
     }
 }
