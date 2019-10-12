f0933b7b46d5051e42bc3a533f3c28281fda4339
danyAmaral <danyamaral27@gmail.com>
2019-10-12 08:31:28 -0300

    usando observable

--- Projetos/app2/src/app/topo/topo.component.ts
+++ Projetos/app2/src/app/topo/topo.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService} from '../ofertas.service';
+import {Oferta} from '../shared/oferta.model';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
-  styleUrls: ['./topo.component.css']
+  styleUrls: ['./topo.component.css'],
+  providers: [OfertasService]
 })
 export class TopoComponent implements OnInit {
-
-  constructor() { }
+  public ofertas: Observable<Oferta[]>
+  constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
+  }
+
+  public pesquisa(pesquisa: string): void{
+      this.ofertas = this.ofertasService.pesquisaOfertas(pesquisa);
+      this.ofertas.subscribe(
+        (ofertas: Oferta[]) => console.log(ofertas))
+
   }
 
 }
