831df9c76df92a8055504ddce8d4fd13dc0f78c2
danielle <danyamaral27@gmail.com>
2019-10-14 09:33:10 -0300

    form inicio

--- Projetos/app2/src/app/app.module.ts
+++ Projetos/app2/src/app/app.module.ts
@@ -12,6 +12,7 @@ import {ROUTES} from './app.routes';
 import { OfertaComponent } from './oferta/oferta.component';
 import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component'
+import {FormsModule} from '@angular/forms'
 import localePT from '@angular/common/locales/pt';
 import localeExtraPT from '@angular/common/locales/extra/pt';
 import { registerLocaleData } from '@angular/common';
@@ -38,6 +39,7 @@ import { OrdemCompraSucessoComponent } from './ordem-compra-sucesso/ordem-compra
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [
--- Projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
+++ Projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -1,89 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service'
-import {Pedido} from '../shared/pedido.model'
-
+import { Pedido } from '../shared/pedido.model'
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: [OrdemCompraService]
+  providers: [ OrdemCompraService ]
 })
 export class OrdemCompraComponent implements OnInit {
 
-  public pedido: Pedido = new Pedido('', '', '', '');
-  public idPedidoCompra: number;
-  public endereco: string = '';
-  public numero: string = '';
-  public complemento: string = '';
-  public formaPagamento: string = '';
-  public enderecoValido: boolean;
-  public numeroValido: boolean;
-  public complementoValido: boolean;
-  public formaPagamentoValido: boolean;
-  public enderecoEstadoPrimitivo: boolean = true;
-  public numeroEstadoPrimitivo: boolean = true;
-  public complementoEstadoPrimitivo: boolean = true;
-  public formaPagamentoEstadoPrimitivo: boolean = true;
-
-  public formEstado: string = 'disabled'
   constructor(private ordemCompraService: OrdemCompraService) { }
 
   ngOnInit() {
-
+    
   }
-
-  public atualizaEndereco(endereco: string): void {
-    this.endereco = endereco;
-    this.enderecoEstadoPrimitivo = false;
-    this.enderecoValido = this.endereco.length > 3;
-    this.habilitaForm();
-  }
-
-  public atualizaNumero(numero: string): void {
-    this.numero = numero;
-    this.numeroEstadoPrimitivo = false;
-    this.numeroValido = this.numero.length > 0
-    this.habilitaForm();
-  }
-
-  public atualizaComplemento(complemento: string): void {
-    this.complemento = complemento;
-    this.complementoEstadoPrimitivo = false;
-    if (this.complemento.length > 0)
-      this.complementoValido = true;
-
-    this.habilitaForm();
-  }
-
-  public atualizaFormaPagamento(formaPagamento: string): void {
-    this.formaPagamento = formaPagamento;
-    this.formaPagamentoEstadoPrimitivo = false;
-    this.formaPagamentoValido = this.formaPagamento.length > 0;
-    this.habilitaForm();
-  }
-
-
-  public habilitaForm(): void {
-    if(this.enderecoValido === true && this.numeroValido === true && this.formaPagamentoValido === true)
-    {
-      this.formEstado = '';
-    }else{
-      this.formEstado = 'disabled';
-    }
-  }
-
-  public confirmarCompra() : void{
-    this.pedido.endereco = this.endereco;
-    this.pedido.numero = this.numero;
-    this.pedido.complemento = this.complemento;
-    this.pedido.formaPagamento = this.formaPagamento;
-
-    this.ordemCompraService.efetivarCompra(this.pedido)
-        .subscribe((resposta) => {
-          console.log(resposta)
-          this.idPedidoCompra = resposta.id;
-        });
-  }
-
 }
