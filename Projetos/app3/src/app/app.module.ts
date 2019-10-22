ff4e4bf305befc6a1208a1c9a4f3536cd49628a6
danyAmaral <danyamaral27@gmail.com>
2019-10-21 22:10:58 -0300

    iniciando animações app3

--- Projetos/app3/src/app/app.module.ts
+++ Projetos/app3/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcessoComponent } from './acesso/acesso.component';
@@ -18,6 +18,7 @@ import { CadastroComponent } from './acesso/cadastro/cadastro.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
